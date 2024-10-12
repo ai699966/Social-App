d47e5f18b79cfee7fed3c47b7b47224c938c45dc
AhmedIbrahim222 <75262647+ai699966@users.noreply.github.com>
2024-10-12 20:45:34 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "scenes/homePage/home";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "themes";
+import Widgets from "scenes/Widgets/widgets";
+
 
 function App() {
+
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)),[mode])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="app">
+    <BrowserRouter>
+    <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/widget" element={<Widgets />} />
+      
+    </Routes>
+    </ThemeProvider>
+    </BrowserRouter>
     </div>
+    </>
   );
 }
 
